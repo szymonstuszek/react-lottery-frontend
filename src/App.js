cf851cb0a0d5debe82c7428517e45d0d20e95475
szymon <szymonstuszek@gmail.com>
2021-11-29 06:06:25 +0100

    Added lottery contract code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import web3 from './web3';
+import lottery from './lottery';
 
 class App extends React.Component {
-  render() {
+    constructor(props) {
+        super(props);
+
+        this.state = { manager: '' }
+    }
+
+    async componentDidMount() {
+        const manager = await lottery.methods.manager().call();
+
+        this.setState({ manager });
+    }
+
+    render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <h2>
             Lottery app
-          </p>
+          </h2>
+          <p> Contract managed by: {this.state.manager} </p>
         </header>
       </div>
     );
-  }
+    }
 }
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address = '0x48bC4B68Ac40B4dF47EA6f8cDE6fAB432f0Dd560';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi, address);
